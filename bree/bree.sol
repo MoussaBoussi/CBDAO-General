0edde6a70e008dda8efb410bf8ecd4c16219037b
CBDAO <68698707+coinbreeder@users.noreply.github.com>
2020-08-04 14:07:20 +0700

    Update bree.sol

--- bree/bree.sol
+++ bree/bree.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT License
 pragma solidity ^0.6.0;
 
 // ----------------------------------------------------------------------------
